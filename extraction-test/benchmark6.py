62372cd46e1b0dbfb04a4c768b1e9d90b24c6501
James Turk <dev@jamesturk.net>
2022-12-15 01:40:33 -0600

    refactor out implementations

---
+++ extraction-test/benchmark6.py
@@ -0,0 +1,75 @@
+import requests
+from implementations import Lxml, BSoup
+
+_cache = {}
+
+
+start_url = "https://docs.python.org/3/genindex-all.html"
+
+
+def cached_get(url):
+    """
+    Use a two-layer cache.
+
+    First, check if the url is in _cache, and return it if so.
+    Second, check if the url is cached to disk, and return it if so.
+    Finally, fetch the URL if needed.
+    """
+    # normalize URL & only fetch URLs that start with library
+    url = url.split("#")[0]
+    if url.startswith("library/"):
+        url = f"https://docs.python.org/3/{url}"
+
+    if url not in _cache:
+        try:
+            with open(f"cache/{url.replace('/', '_')}", "rb") as f:
+                _cache[url] = f.read()
+        except FileNotFoundError:
+            pass
+        response = requests.get(url)
+        _cache[url] = response.content
+        with open(f"cache/{url.replace('/', '_')}", "wb") as f:
+            f.write(response.content)
+    return _cache[url]
+
+
+def time_decorator(func):
+    import time
+
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        func(*args, **kwargs)
+        end = time.perf_counter()
+        print(f"took {end - start} seconds")
+
+    return wrapper
+
+
+@time_decorator
+def scrape(impl):
+    nodes = 0
+    text_count = 0
+    subpages = 0
+    spans = 0
+
+    html = cached_get(start_url)
+    tree = impl.parse_dom(html)
+    for link in impl.find_tags(tree, "a"):
+        href = link.get("href").split("#")[0]
+        if href.startswith("library/"):
+            subpage = impl.parse_dom(cached_get(href))
+            subpages += 1
+            nodes += len(impl.all_nodes(subpage))
+            text_count += len(impl.all_text(subpage))
+            spans += len(list(impl.find_tags(subpage, "span")))
+
+    print(
+        f"lxml scraped {nodes} nodes, {spans} <span> tags, and {text_count} characters from {subpages} subpages"
+    )
+
+
+scrape(Lxml())  # prime cache
+
+# scrape(BSoup("html.parser"))
+scrape(BSoup("lxml"))
+# scrape(BSoup("html5lib"))
---
+++ extraction-test/implementations.py
@@ -0,0 +1,113 @@
+from abc import ABC
+import pathlib
+import lxml.html
+from bs4 import BeautifulSoup, Tag
+
+
+class Base(ABC):
+    examples = ["asha_bhosle", "html5test", "nba", "python"]
+
+    def __init__(self):
+        self.html = {}
+        for example in self.examples:
+            self.html[example] = (
+                pathlib.Path("testdata") / f"{example}.html"
+            ).read_text()
+
+    def pre_parse(self):
+        # Pre-parse the HTML to allow us to benchmark individual selections
+        self.root = {}
+        for name, html in self.html.items():
+            self.root[name] = self.load_dom(name)
+
+
+class Lxml(Base):
+    def parse_dom(self, html):
+        return lxml.html.fromstring(html)
+
+    def find_tags(self, tree, tag):
+        return tree.xpath(f"//{tag}")
+
+    def all_nodes(self, tree):
+        return [e for e in tree.iter()]
+
+    def all_text(self, tree):
+        return tree.text_content()
+
+    # benchmarks 1-5
+
+    def load_dom(self, example):
+        return lxml.html.fromstring(self.html[example])
+
+    def links_natural(self, example):
+        return self.root[example].xpath("//a/@href")
+
+    def links_css(self, example):
+        return self.root[example].cssselect("a[href]")
+
+    def count_elements(self, example):
+        elements = []
+
+        def count(element):
+            if isinstance(element, lxml.html.HtmlElement):
+                elements.append(element.tag)
+                for child in element.getchildren():
+                    count(child)
+
+        count(self.root[example])
+        return elements
+        # return list(self.root[example].iter())
+
+    def extract_text(self, example):
+        return self.root[example].text_content()
+
+    def __repr__(self):
+        return "lxml.html"
+
+
+class BSoup(Base):
+    def __init__(self, parser):
+        super().__init__()
+        self.parser = parser
+
+    def parse_dom(self, html):
+        return BeautifulSoup(html, self.parser)
+
+    def find_tags(self, tree, tag):
+        return tree.find_all(tag)
+
+    def all_nodes(self, tree):
+        return [e for e in tree.recursiveChildGenerator() if isinstance(e, Tag)]
+
+    def all_text(self, tree):
+        return tree.get_text()
+
+    # benchmarks 1-5
+
+    def load_dom(self, example):
+        return BeautifulSoup(self.html[example], self.parser)
+
+    def links_natural(self, example):
+        return self.root[example].find_all("a", href=True)
+
+    def links_css(self, example):
+        return self.root[example].select("a[href]")
+
+    def count_elements(self, example):
+        elements = []
+
+        def count(element):
+            if isinstance(element, Tag):
+                elements.append(element.name)
+                for child in getattr(element, "children", []):
+                    count(child)
+
+        count(self.root[example])
+        return elements
+        # return list(self.root[example].recursiveChildGenerator())
+
+    def extract_text(self, example):
+        return self.root[example].get_text()
+
+    def __repr__(self):
+        return f"BeautifulSoup[{self.parser}]"
